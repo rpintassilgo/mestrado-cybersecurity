7663f696acf9e5c6beaa9663ba05dd43fa59d857
rpintassilgo <rodrigofpintassilgo@gmail.com>
2024-01-20 02:25:42 +0000

    chore(server): configure promclient and elasticsearch

--- weather-information/weather-backend/src/config/passport.js
+++ weather-information/weather-backend/src/config/passport.js
@@ -2,6 +2,33 @@ const passport = require('passport')
 const bcrypt = require('bcryptjs')
 const User = require('../models/User');
 const LocalStrategy = require('passport-local').Strategy
+const prometheus = require('prom-client');
+const winston = require('winston');
+const { ElasticsearchTransport } = require('winston-elasticsearch');
+
+const esTransportOpts = {
+  level: 'info',
+  clientOpts: {
+    // elasticsearch docker's ip
+    node: "http://172.19.0.3:9200"
+  },
+  indexPrefix: 'app-logs-*',
+  index: `app-logs-${new Date().toISOString().split('T')[0]}`
+};
+
+const esTransport = new ElasticsearchTransport(esTransportOpts);
+
+const logger = winston.createLogger({
+  transports: [
+    esTransport
+  ],
+});
+
+const failedLoginCounter = new prometheus.Counter({
+    name: 'failed_login_attempts_total',
+    help: 'Total number of failed login attempts',
+    labelNames: ['email'],
+});  
 
 passport.serializeUser((user, done) => {
     done(null, user.email)
@@ -18,7 +45,7 @@ passport.deserializeUser(async (email, done) => {
 
 passport.use(new LocalStrategy({
     usernameField: 'email',
-    passportField: 'password'
+    passwordField: 'password'
 }, async (username, password, done) => {
     try {
         const user = await User.findOne({ email: username }).select('+password')
@@ -26,8 +53,14 @@ passport.use(new LocalStrategy({
         if(!user) done(null,false)
     
         if(bcrypt.compareSync(password, user.password)){
+            // Passwords match
             done(null,user)
         } else{
+            // Passwords do not match
+            const timestamp = new Date().toISOString();
+            const message = `Failed login attempt for user: ${username}`;
+            logger.error({ message, '@timestamp': timestamp });
+            failedLoginCounter.inc({ email: username });
             done(null,false)
         }          
     } catch (error) {
